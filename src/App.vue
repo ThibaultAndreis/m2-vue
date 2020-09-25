16dccd37fedb7adb98631cd01c062c33387b0fd9
thibault <thibault.andreis@gmail.com>
2020-09-25 11:41:23 +0200

    exercie don

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,36 @@
 <template>
   <div id="app">
+    <div class="moon"></div>
+    <div v-for="etoile in etoiles" class="star" v-bind:style="{ left: randomX() + 'px', top: randomY() + 'px'}">
 
+    </div>
   </div>
 </template>
 
 <script>
+import {randomX,randomY }from "./helpers/random"
+
 export default {
-  name: "app"
+  name: "app",
+  data() {
+    return {
+      etoiles : 200,
+      randomX,
+      randomY
+    }
+  }
 };
+
+
+
+
 </script>
 
 <style lang="scss">
 body {
   margin: 0;
 }
+
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -39,7 +56,7 @@ body {
   width: 100px;
   background-image: url("https://3dexport.com/items/2015/02/20/385337/100201/moon_texture_3d_model_c4d_max_obj_fbx_ma_lwo_3ds_3dm_stl_1258985_o.png");
   background-size: cover;
-  box-shadow: 0px 0px 12px 4px white;
+  box-shadow: 0 0 12px 4px white;
   filter: brightness(1.5);
   z-index: 1;
   right: 10%;
